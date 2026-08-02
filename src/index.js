9b1ff984dd1b8d5887a5b1a9b10a7a1477b3fbb3
Ali Farooq <m.aliafaroq@gmail.com>
2026-08-01 19:22:34 -0700

    Fixed location button and inconsistant semicolons
    Fixed the location button being written as "LOC4710N" instead of "10C4710N" also added semicolons to some sections of index.js that were missing it.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import "./styles.css";
-import krabbyImage from "./chrome-krabby-patty.png"
-import kelpImg from "./chrome-kelp-shake.png"
-import krustyKrabImg from "./chrome-krusty-krab.png"
+import krabbyImage from "./chrome-krabby-patty.png";
+import kelpImg from "./chrome-kelp-shake.png";
+import krustyKrabImg from "./chrome-krusty-krab.png";
 
 
-let content = document.querySelector("#content")
-let homeButton = document.querySelector("#home-button")
-let menuButton = document.querySelector("#menu-button")
-let locationButton = document.querySelector("#location-button")
+let content = document.querySelector("#content");
+let homeButton = document.querySelector("#home-button");
+let menuButton = document.querySelector("#menu-button");
+let locationButton = document.querySelector("#location-button");
 
 //Home Button Functionality
 homeButton.addEventListener("click", () => {
@@ -91,7 +91,7 @@ homeButton.addEventListener("click", () => {
     hours.appendChild(hourListItemSeven);
 });
 
-homeButton.click() // Initializes Home Screen on Page Start
+homeButton.click(); // Initializes Home Screen on Page Start
 
 menuButton.addEventListener("click", () => {
     content.innerHTML = ""; // Clears the child elements of the content div
@@ -109,73 +109,73 @@ menuButton.addEventListener("click", () => {
 
     //Item 1 Element
     let item1 = document.createElement("div");
-    item1.setAttribute("class", "item1")
-    menu.appendChild(item1)
+    item1.setAttribute("class", "item1");
+    menu.appendChild(item1);
 
     //Image of Chrome Krabby Patty
-    let imgKrabby = document.createElement("img")
-    imgKrabby.setAttribute("class", "img-krabby")
-    imgKrabby.setAttribute("src", krabbyImage)
-    imgKrabby.setAttribute("alt", "7H3 W0ND3RFU1 CHR0M3 KR488Y P477Y")
-    item1.appendChild(imgKrabby)
+    let imgKrabby = document.createElement("img");
+    imgKrabby.setAttribute("class", "img-krabby");
+    imgKrabby.setAttribute("src", krabbyImage);
+    imgKrabby.setAttribute("alt", "7H3 W0ND3RFU1 CHR0M3 KR488Y P477Y");
+    item1.appendChild(imgKrabby);
 
     //Chrome Krabby Patty Title
-    let pattyTitle = document.createElement("h2")
-    pattyTitle.setAttribute("class", "patty")
-    pattyTitle.textContent = "CHR0M3 KR488Y P477Y"
-    item1.appendChild(pattyTitle)
+    let pattyTitle = document.createElement("h2");
+    pattyTitle.setAttribute("class", "patty");
+    pattyTitle.textContent = "CHR0M3 KR488Y P477Y";
+    item1.appendChild(pattyTitle);
 
     //Chrome Krabby Patty Description
-    let pattyDescription = document.createElement("p")
-    pattyDescription.textContent = "4 CU570M3R F4V0R173, 7H3 CHR0M3 KR488Y P477Y 15 3NJ0Y3D 8Y 411."
-    item1.appendChild(pattyDescription)
+    let pattyDescription = document.createElement("p");
+    pattyDescription.textContent = "4 CU570M3R F4V0R173, 7H3 CHR0M3 KR488Y P477Y 15 3NJ0Y3D 8Y 411.";
+    item1.appendChild(pattyDescription);
 
     //Item 2 Element
     let item2 = document.createElement("div");
-    item2.setAttribute("class", "item2")
-    menu.appendChild(item2)
+    item2.setAttribute("class", "item2");
+    menu.appendChild(item2);
 
     //Image of Chrome Kelp Shake
-    let imgKelpShake = document.createElement("img")
-    imgKelpShake.setAttribute("class", "img-kelp-shake")
-    imgKelpShake.setAttribute("src", kelpImg) //Just learn you can do (img.src = "") ... SUCH A GREAT TIME SAVE THOUGH THIS WAS SO ANNOYING, but I continue until location button
-    imgKelpShake.setAttribute("alt", "7H3 R3FR35H1N9 K31P 5H4K3")
-    item2.appendChild(imgKelpShake)
+    let imgKelpShake = document.createElement("img");
+    imgKelpShake.setAttribute("class", "img-kelp-shake");
+    imgKelpShake.setAttribute("src", kelpImg); //Just learn you can do (img.src = "") ... SUCH A GREAT TIME SAVE THOUGH THIS WAS SO ANNOYING, but I continue until location button
+    imgKelpShake.setAttribute("alt", "7H3 R3FR35H1N9 K31P 5H4K3");
+    item2.appendChild(imgKelpShake);
 
     //Chrome Kelp Shake Title
-    let kelpShake = document.createElement("h2")
-    kelpShake.setAttribute("class", "kelp-shake")
-    kelpShake.textContent = "CHR0M3 K31P 5H4K3"
-    item2.appendChild(kelpShake)
+    let kelpShake = document.createElement("h2");
+    kelpShake.setAttribute("class", "kelp-shake");
+    kelpShake.textContent = "CHR0M3 K31P 5H4K3";
+    item2.appendChild(kelpShake);
 
     //Chrome Kelp Shake Description
-    let kelpShakeDescription = document.createElement("p")
-    kelpShakeDescription.textContent = "7H3 8357 7H1N9 70 90 W17H 4 CHR0M3 KR488Y P477Y."
-    item2.appendChild(kelpShakeDescription)
-})
+    let kelpShakeDescription = document.createElement("p");
+    kelpShakeDescription.textContent = "7H3 8357 7H1N9 70 90 W17H 4 CHR0M3 KR488Y P477Y.";
+    item2.appendChild(kelpShakeDescription);
+});
 locationButton.addEventListener("click", () => {
     content.innerHTML = ""; // Clears the child elements of the content div
 
     //Location Title Element --- just started to change formatting for practice
-    let locationTitle = document.createElement("h2")
-    locationTitle.id = "title"
-    locationTitle.textContent = "0UR 570R3 10C4710N:"
-    content.appendChild(locationTitle)
+    let locationTitle = document.createElement("h2");
+    locationTitle.id = "title";
+    locationTitle.textContent = "0UR 570R3 10C4710N:";
+    content.appendChild(locationTitle);
     
     //Location Element
-    let location = document.createElement("div")
-    location.id = "location"
-    content.appendChild(location)
+    let location = document.createElement("div");
+    location.id = "location";
+    content.appendChild(location);
 
     //Chrome Krusty Krab Img
-    let krustyKrab = document.createElement("img")
-    krustyKrab.src = krustyKrabImg
-    krustyKrab.className = "krusty-krab"
-    krustyKrab.alt = "4 834U71FU1 1M463 0F KRU57Y KR48."
-    location.appendChild(krustyKrab)
+    let krustyKrab = document.createElement("img");
+    krustyKrab.src = krustyKrabImg;
+    krustyKrab.className = "krusty-krab";
+    krustyKrab.alt = "4 834U71FU1 1M463 0F KRU57Y KR48.";
+    location.appendChild(krustyKrab);
     
     //Location text
-    let locationDesc = document.createElement("p")
-    locationDesc.innerHTML = "<strong>7313P0R7 70:</strong> 831 80770M F33D3R 1N. 81K1N1 80770M, P4C1F1C 0C34N <strong>(7313P0R7 C0D3: KK8357).</strong>"
-    location.appendChild(locationDesc)
-})
+    let locationDesc = document.createElement("p");
+    locationDesc.innerHTML = "<strong>7313P0R7 70:</strong> 831 80770M F33D3R 1N. 81K1N1 80770M, P4C1F1C 0C34N <strong>(7313P0R7 C0D3: KK8357).</strong>";
+    location.appendChild(locationDesc);
+});
